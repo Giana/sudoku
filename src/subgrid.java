dce1b6670c1d0b6e1f6663462a0c0786eb4f4038
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-06-27 22:34:49 -0400

    Getters and setters for subgrid

--- src/subgrid.java
+++ src/subgrid.java
@@ -1,3 +1,18 @@
 public class subgrid
 {
+    tile[][] values = new tile[3][3];
+    boolean revealed;
+    boolean correct;
+
+    public int getValue(int x, int y) { return this.values[x][y].getValue(); };
+
+    public void setValue(int x, int y, int value) { this.values[x][y].setValue(value); }
+
+    public boolean getRevealed() { return this.revealed; }
+
+    public void setRevealed(boolean revealed) { this.revealed = revealed; }
+
+    public boolean getCorrect() { return this.correct; }
+
+    public void setCorrect(boolean correct) { this.correct = correct; }
 }
