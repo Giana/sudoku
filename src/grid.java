988ae4f904308fa7ba505317a9cbc505377aa886
gnguarascio <42662834+gnguarascio@users.noreply.github.com>
2020-06-28 09:47:20 -0400

    Getters and setters for grid

--- src/grid.java
+++ src/grid.java
@@ -1,3 +1,16 @@
 public class grid
 {
+    subgrid[][] subgrids = new subgrid[3][3];
+    boolean revealed;
+    boolean correct;
+
+    public subgrid getSubgrid(int x, int y) { return this.subgrids[x][y]; };
+    
+    public boolean getRevealed() { return this.revealed; }
+
+    public void setRevealed(boolean revealed) { this.revealed = revealed; }
+
+    public boolean getCorrect() { return this.correct; }
+
+    public void setCorrect(boolean correct) { this.correct = correct; }
 }
